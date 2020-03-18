9babf20b1145b0023e8a877e9538ffce34143052
Tanner Hill <thill17@wou.edu>
2020-03-18 14:19:03 -0700

    improved model

--- index.js
+++ index.js
@@ -1,82 +1,55 @@
 $(document).ready(() => {
 
-    var circleRadius = 5;
+    var bloodCellRadius = 5;
 
     var virusRadius = 2;
 
-    var defenderRatio = 0.2;
+    var defenderRadius = 3;
+
+    var defenderRatio = 0.1;
+
+    var defenseDistance = 10;
+
+    var followDistance = 400;
+
+    var infectionDistance = 10;
+
+    var bloodCellStartCount = 50;
+
+    var virusStartCount = 4;
+
+    var defenderSpawnRate = 5000;
+
+    var defenderLifeSpan = 5000;
+
+    var bloodCellLimit = 1000;
+
+    var bloodCellLifespan = 10000;
+
+    var maxDefenderRatio = 0.5;
 
     var canvas = document.querySelector("#canvas");
     var ctx = canvas.getContext("2d");
-
-    var globalAgentCount = 200;
 
     var xLimit = canvas.width;
     var yLimit = canvas.height;
 
     var vmax = xLimit > yLimit ? (xLimit / 100) : (yLimit / 100);
 
-    var hole = {
-        x: (canvas.width / 4) * 3   ,
-        yStart: (canvas.height / 6) * 2,
-        yEnd: (canvas.height / 6) * 4
-    }
-
-    class Agent {
-        constructor({xPos,yPos,isVirus,radius,id,isDefender}) {
-            this.isVirus = isVirus;
-            this.isInfected = false;
-            this.isDefender = isDefender;
-            this.position = {xPos,yPos};
-            this.color = isVirus ? "red" : (this.isDefender ? "blue" : "green");
+    class Cell {
+        constructor({x,y,radius,id}) {
+            this.pos = {x,y};
+            this.color = "red";
             this.angle = Math.random() * 360;
-            this.speed = 8;
             this.id = id;
-            this.radius = radius;
         }
 
-        getNeighbors(agents) {
+        getNeighbors(agents,dist) {
             var neighbors = agents.filter(a => {
-                let distance = getDistance(this.position.xPos,a.position.xPos,this.position.yPos,a.position.yPos);
-                return a.elementID != this.elementID && distance < 150;
+                let distance = getDistance(this.pos.x,a.pos.x,this.pos.y,a.pos.y);
+                return a.elementID != this.elementID && distance < dist;
             });
             return neighbors;
-        }
-
-        checkVirus() {
-            var {xPos, yPos} = this.position;
-            viruses.forEach(v => {
-                if(getDistance(xPos,v.position.xPos,yPos,v.position.yPos) < 5) {
-                    if(!this.isDefender){
-                        if(Math.random() < 0.1) {
-                            this.infect(viruses);
-                            return false;
-                        }
-                    }
-                    else {
-                        if(Math.random() < 0.3) {
-                            viruses = viruses.filter(vi => vi.id != v.id);
-                            $("#virusCount").text(viruses.length);
-                        }
-                    }
-                }
-            });
-        }
-
-        infect() {
-            //console.log("infected!");
-            this.isInfected = true;
-            this.isDefender = false;
-            this.color = "yellow";
-            var {xPos, yPos} = this.position;
-            var id = this.id;
-            setTimeout(() => {
-                for(let i = 0; i < 2; i++) {
-                    viruses = [...viruses,new Agent({xPos,yPos,id:"virus"+viruses.length, isVirus: true,radius:virusRadius})]
-                }
-                $("#virusCount").text(viruses.length);
-                removeAgent(id);
-            },5000);
         }
 
         draw(newXPos,newYPos) {
@@ -85,12 +58,13 @@ $(document).ready(() => {
                 ctx.beginPath();
                 ctx.fillStyle = this.color;
                 ctx.strokeStyle = this.color;
-                ctx.moveTo(newXPos,newYPos);
-                ctx.arc(this.position.xPos,this.position.yPos,this.radius,0,2*Math.PI);
+                ctx.moveTo(this.pos.x,this.pos.y);
+                ctx.arc(this.pos.x,this.pos.y,this.radius,0,2*Math.PI);
+                ctx.fill();
                 return;
             }
-            this.position.xPos = newXPos;
-            this.position.yPos = newYPos;
+            this.pos.x = newXPos;
+            this.pos.y = newYPos;
             ctx.beginPath();
             ctx.fillStyle = this.color;
             ctx.strokeStyle = this.color;
@@ -98,6 +72,99 @@ $(document).ready(() => {
             ctx.arc(newXPos,newYPos,this.radius,0,2*Math.PI);
             ctx.fill();
         }
+
+    }
+
+    class BloodCell extends Cell {
+        constructor({x,y,id}) {
+            super({x,y,id});
+            this.isInfected = false;
+            this.radius = bloodCellRadius;
+            this.angle = Math.random() * 360;
+            this.speed = 8;
+            this.radius = bloodCellRadius;
+        }
+
+        checkVirus() {
+            var {x, y} = this.pos;
+            agents.filter(a => a instanceof Virus).forEach(v => {
+                if(getDistance(x,v.pos.x,y,v.pos.y) < infectionDistance && !this.isInfected) {
+                    if(Math.random() < 0.1) {
+                        this.infect();
+                        return false;
+                    }
+                }
+                
+            });
+        }
+
+        infect() {
+            this.isInfected = true;
+            this.color = "yellow";
+            var {x, y} = this.pos;
+            var id = this.id;
+            setTimeout(() => {
+                for(let i = 0; i < 2; i++) {
+                    agents = [...agents,new Virus({x,y,id:"virus"+agents.filter(a => a instanceof Virus).length})]
+                }
+                $("#virusCount").text(agents.filter(a => a instanceof Virus).length);
+                removeAgent(id);
+            },5000);
+        }
+
+        divide() {
+            var bloodCellCount = agents.filter(a => a instanceof BloodCell).length;
+            if(bloodCellCount < bloodCellLimit) {
+                agents = [...agents,new BloodCell({x:this.pos.x,y:this.pos.y,id:"bloodcell"+bloodCellCount})];
+                $("#bloodCellCount").text(agents.filter(a => a instanceof BloodCell).length);
+            }
+            
+        }
+    }
+
+    class Virus extends Cell {
+        constructor({x,y,id}) {
+            super({x,y,id});
+            this.color = "green";
+            this.angle = Math.random() * 360;
+            this.speed = 10;
+            this.radius = virusRadius;
+        }
+    }
+
+    class Defender extends Cell {
+        constructor({x,y,id}) {
+            super({x,y,id});
+            this.color = "blue";
+            this.angle = Math.random() * 360;
+            this.speed = 8;
+            this.radius = defenderRadius;
+        }
+
+        checkVirus() {
+            var {x, y} = this.pos;
+            agents.filter(a => a instanceof Virus).forEach(v => {
+                var dist = getDistance(x,v.pos.x,y,v.pos.y);
+                if(dist < followDistance) {
+                    this.angle = getFollowAngle(x,v.pos.x,y,v.pos.y);
+                    if(dist < defenseDistance) {
+                        if(Math.random() < 0.3) {
+                            if(agents.filter(a => a instanceof Defender).length < Math.floor(maxDefenderRatio * bloodCellStartCount)) {
+                                spawnWhiteBloodCell();
+                            } 
+                            removeAgent(v.id);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    function getFollowAngle(x1,x2,y1,y2) {
+        var angle = Math.atan2(y1-y2,x1-x2);
+        angle = (180 / Math.PI) * angle;
+        if (angle < 0) angle = 360 + angle;
+        return angle; 
     }
 
     function getDistance(x1, x2, y1, y2) {
@@ -109,8 +176,15 @@ $(document).ready(() => {
 
     function removeAgent(id) {
         agents = agents.filter(a => a.id != id);
-        globalAgentCount--;
-        $("#agentCount").text(globalAgentCount);
+        $("#bloodCellCount").text(agents.filter(a => a instanceof BloodCell).length);
+        $("#virusCount").text(agents.filter(a => a instanceof Virus).length);
+    }
+
+    function spawnWhiteBloodCell() {
+        let randX = Math.random() * xLimit;
+        let randY = Math.random() * yLimit;
+        agents = [...agents,new Defender({x:randX,y:randY,id:"defender"+agents.filter(a => a instanceof Defender).length})]
+        
     }
 
     function moveRandom(agents) {
@@ -125,47 +199,40 @@ $(document).ready(() => {
         ctx.clearRect(0,0,canvas.width,canvas.height);
         ctx.beginPath();
         agents.forEach(agent => {
-            let neighbors = agent.getNeighbors(agents);
-            neighbors.forEach(neighbor => {
-                if(getDistance(neighbor.position.xPos, agent.position.xPos, neighbor.position.yPos, agent.position.yPos) < circleRadius) {
-                    let tempAngle = neighbor.angle;
-                    neighbor.angle = agent.angle;
-                    agent.angle = tempAngle;
-                }
-            });
-            if(neighbors.length > 2) {
-                agent.angle = Math.random() * 360;
-            }
-            let newX = agent.position.xPos + agent.speed * Math.sin(agent.angle);
-            let newY = agent.position.yPos - agent.speed * Math.cos(agent.angle);
+            let newX = agent.pos.x + agent.speed * Math.sin(agent.angle);
+            let newY = agent.pos.y - agent.speed * Math.cos(agent.angle);
             //console.log({newX,newY});
             agent.draw(newX,newY);
-            if(!agent.isInfected) {
-                agent.checkVirus(viruses);
+            if(!(agent instanceof Virus)) {
+                agent.checkVirus();
             }
-        });
-        ctx.beginPath();
-        viruses.forEach(v => {
-            let newX = v.position.xPos + v.speed * Math.sin(v.angle);
-            let newY = v.position.yPos - v.speed * Math.cos(v.angle);
-            //console.log({newX,newY});
-            v.draw(newX,newY);
+            if(agent instanceof BloodCell && agents.filter(a => a instanceof BloodCell).length < bloodCellStartCount && !agent.isInfected) {
+                agent.divide();
+            }
         });
     }
 
     var agents = [];
-    var viruses = [];
     
-    $("#agentCount").text(globalAgentCount);
-    $("#virusCount").text(1);
+    $("#bloodCellCount").text(bloodCellStartCount);
+    $("#virusCount").text(virusStartCount);
 
-    for(let i = 0; i < globalAgentCount; i++) {
+    for(let i = 0; i < bloodCellStartCount; i++) {
         let randX = (Math.random() * xLimit);
         let randY = (Math.random() * yLimit);
-        agents = [...agents,new Agent({xPos:randX,yPos:randY, id: "agent"+i, radius:circleRadius, isDefender: i < (globalAgentCount * defenderRatio)})];
+        if(i < bloodCellStartCount * defenderRatio) {
+            agents = [...agents,new Defender({x:randX,y:randY,id:"defender"+i})];
+        }
+        else {
+            agents = [...agents,new BloodCell({x:randX,y:randY,id:"bloodcell"+i})];
+        }
     }
 
-    viruses = [new Agent({xPos:Math.random() * xLimit, yPos:Math.random() * yLimit,isVirus:true,id:"virus"+0,radius:virusRadius})];
+    for(let i = 0; i < virusStartCount; i++) {
+        let randX = (Math.random() * xLimit);
+        let randY = (Math.random() * yLimit);
+        agents = [...agents,new Virus({x:randX,y:randY,id:"virus"+i})];
+    }
 
     var moveAgentsRandomly = setInterval(() => {
         moveAgents();
